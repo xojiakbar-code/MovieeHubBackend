80f1f7115eeb0d10c9d7380a45f482f24e57408b
Xojiakbar Xasanboyev | Full-Stack Middle Developer <xojiakbarxasanboyevdev@gmail.com>
2026-08-29 09:52:46 +0500

    Create Admin.js

---
+++ backend/models/Admin.js
@@ -0,0 +1,38 @@
+// Admin foydalanuvchi modeli
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+// Parolni hash qilish (saqlashdan oldin)
+adminSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Parolni tekshirish metodi
+adminSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('Admin', adminSchema);
